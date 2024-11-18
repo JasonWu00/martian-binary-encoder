e9d480a693131581fee0e495181fcbcf3d1e37b0
jasonzehongwu <jasonzehongwu@gmail.com>
2024-11-17 20:16:13 -0500

    updated to streamlit standards

--- english_to_boxes.py
+++ english_to_boxes.py
@@ -1,10 +1,30 @@
 """
+
+Copyright (C) 2024 Jason Wu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+
 This Python file defines two functions, an encoder and a decoder,
 that converts plaintext into spooky blackbox blocks and back.
 """
 
-def encoder():
-    user_input = input("Enter a string to be black-box-ified: ")
+import streamlit as st
+from st_copy_to_clipboard import st_copy_to_clipboard
+
+def encoder(user_input: str):
+    #user_input = input("Enter a string to be black-box-ified: ")
     output = ""
 
     zero = '\u2591'
@@ -31,10 +51,11 @@ def encoder():
                     output += two
                 case '11':
                     output += three
-    print(output)
+    #print(output)
+    return(output)
 
-def decoder():
-    user_input = input("Enter faux-binary string to be decoded: ")
+def decoder(user_input: str):
+    #user_input = input("Enter faux-binary string to be decoded: ")
     output = ""
     char_bin = ""
 
@@ -55,14 +76,43 @@ def decoder():
             char_ascii = int(char_bin, 2)
             output += chr(char_ascii)
             char_bin = ""
-    print(output)
+    #print(output)
+    return(output)
 
 def main():
-    user_choice = input("Encode or decode? ")
-    if user_choice.lower() == "encode":
-        encoder()
-    elif user_choice.lower() == "decode":
-        decoder()
-    else: print("That is not a legitimate option")
 
-main()
+    st.markdown(
+        """
+        <style>
+            .main > div {
+                padding-left: 2.5rem;
+                padding-right: 2.5rem;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.markdown("Martian Binary encoder and decoder thing",unsafe_allow_html=True)
+
+    option = st.selectbox(
+        "Do you want to encode or decode?",
+        ("Encode", "Decode"),
+    )
+    user_input = st.text_input("Enter a string or faux-Binary:")
+
+    if st.button("Carry out encode/decode"):
+        if option == "Encode":
+            output = encoder(user_input)
+        elif option == "Decode":
+            output = decoder(user_input)
+        else:
+            output = "I have no idea what you did but you managed to break the options."
+
+        st.markdown("Your encoded or decoded text is:")
+        st.code(output)
+        st.markdown("Click here to copy the text:")
+        st_copy_to_clipboard(output)
+
+if __name__ == "__main__":
+    main()
